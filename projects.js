96986d768572c7443d06c6453f566cfcdf95a4ad
Akulinichev Maxim <mxmrus@gmail.com>
2019-11-30 10:19:33 +0300

    refactor: code styling

--- projects.js
+++ projects.js
@@ -17,14 +17,14 @@ document.getElementById('back').addEventListener('click', () => {
 document.getElementById('to_mobile').addEventListener('click', () => {
   let frame = document.querySelector('.frame');
   if (sessionStorage.getItem('project') === 'theyalow') {
-      frame.setAttribute('width','640px');
-      document.querySelectorAll('.button').forEach(element => element.classList.add('button-to_mobile_640'));
-    } else {
-        frame.setAttribute('width','375px'); 
-        document.querySelectorAll('.button').forEach(element => element.classList.add('button-to_mobile_375'));
-    }
-    document.getElementById('to_mobile').classList.add('button-hidden');
-    document.getElementById('to_desktop').classList.remove('button-hidden');
+    frame.setAttribute('width','640px');
+    document.querySelectorAll('.button').forEach(element => element.classList.add('button-to_mobile_640'));
+  } else {
+      frame.setAttribute('width','375px'); 
+      document.querySelectorAll('.button').forEach(element => element.classList.add('button-to_mobile_375'));
+  }
+  document.getElementById('to_mobile').classList.add('button-hidden');
+  document.getElementById('to_desktop').classList.remove('button-hidden');
 })
 document.getElementById('to_desktop').addEventListener('click', () => {
   let frame = document.querySelector('.frame');
